209e3983c3bfbf8517b1f989432c0892ddbdb3ad
DanielDimitrov1 <dimitrovvdaniel@gmail.com>
2025-02-07 14:45:44 +0200

    misspelled chechks

--- scripts/setup_authentication.sh
+++ scripts/setup_authentication.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 default_authentication() {
-  echo "Using default authentication with AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY." 
+  echo "Using default authentication with AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY."
   AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:?'AWS_ACCESS_KEY_ID variable missing.'}
-  AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?'AWS_SECRET_ACCESS_KEY variable missing. '}
+  AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?'AWS_SECRET_ACCESS_KEY variable missing.'}
 }
 
 oidc_authentication() {
   echo "Authenticating with an OpenID Connect (OIDC) Web Identity Provider."
   mkdir -p /.aws-oidc
-  AWS_WEB_IDENTITY_TOKEN_FILE=/.aws-oidc/web_identity_token 
+  AWS_WEB_IDENTITY_TOKEN_FILE=/.aws-oidc/web_identity_token
   echo "${BITBUCKET_STEP_OIDC_TOKEN}" >> ${AWS_WEB_IDENTITY_TOKEN_FILE}
   chmod 400 ${AWS_WEB_IDENTITY_TOKEN_FILE}
   aws configure set web_identity_token_file ${AWS_WEB_IDENTITY_TOKEN_FILE}
@@ -28,16 +28,16 @@ EOF
 }
 
 sts_authentication() {
-  echo "Authenticating with STS role." 
+  echo "Authenticating with STS role."
   mkdir -p /.aws-sts
   AWS_STS_CREDENTIALS_FILE=/.aws-sts/credentials
   aws sts assume-role --role-arn ${AWS_ROLE_ARN} --role-session-name ${AWS_ROLE_SESSION_NAME} > ${AWS_STS_CREDENTIALS_FILE}
   chmod 400 ${AWS_STS_CREDENTIALS_FILE}
   AWS_ACCESS_KEY_ID=$(cat ${AWS_STS_CREDENTIALS_FILE} | jq -r '.Credentials.AccessKeyId')
   AWS_SECRET_ACCESS_KEY=$(cat ${AWS_STS_CREDENTIALS_FILE} | jq -r '.Credentials.SecretAccessKey')
-  AWS_SESSION_TOKEN=$(cat ${AWS_STS_CREDENTIALS_FILE} | jq -r '.Credentials.SessionToken') 
+  AWS_SESSION_TOKEN=$(cat ${AWS_STS_CREDENTIALS_FILE} | jq -r '.Credentials.SessionToken')
   export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-  export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} 
+  export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
   export AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
 }
 
@@ -45,12 +45,12 @@ setup_authentication() {
 
   AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:?'AWS_DEFAULT_REGION variable missing.'}
   AWS_OIDC_ROLE_ARN=${1:?'OIDC role ARN argument missing.'}
- 
+
   if [[ -n "${AWS_OIDC_ROLE_ARN}" ]]; then
     if [[ -n "${BITBUCKET_STEP_OIDC_TOKEN}" ]]; then
       oidc_authentication
     else
-      echo "Parameter 'oide: true' in the step configuration is required for OIDC authentication" 
+      echo "Parameter 'oide: true' in the step configuration is required for OIDC authentication"
       default_authentication
     fi
   elif [[ -n "${AWS_ROLE_ARN}" ]]; then
@@ -59,7 +59,7 @@ setup_authentication() {
     default_authentication
   fi
 
-# Export variables to AWS credentials file as default profile#
+  # Export variables to AWS credentials file as default profile#
   mkdir -p ~/.aws
   cat <<EOF > ~/.aws/credentials
 [default]
