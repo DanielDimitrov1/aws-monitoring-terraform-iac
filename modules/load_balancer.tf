e1ab63bf1c2229152a63547c201f3e21e49e8239
DanielDimitrov1 <dimitrovvdaniel@gmail.com>
2025-02-24 01:50:47 +0200

    modified

--- modules/load_balancer.tf
+++ modules/load_balancer.tf
@@ -1,3 +1,5 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
 
 resource "aws_cloudwatch_metric_alarm" "lb_4xx_error" {
   alarm_name                = "Healthy-Count"
@@ -8,15 +10,12 @@ resource "aws_cloudwatch_metric_alarm" "lb_4xx_error" {
   period                    = "60"
   statistic                 = "Average"
   threshold                 = "60"
-
   alarm_description         = "This alarm will notify us when there is 1 or more unhealthy hosts."
-
   alarm_actions = [aws_sns_topic.alarm.arn]
   ok_actions    = [aws_sns_topic.alarm.arn]
-
   treat_missing_data = "notBreaching"
-  dimensions = {
-    LoadBalancer = var.alb_arn}
+  dimensions = "arn:aws:elasticloadbalancing:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:loadbalancer/app/main/f97903b029d2ff71"
+
 }
 
 #data "aws_lb" "main" {
